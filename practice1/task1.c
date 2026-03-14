2fefdfb97c8618fb35d94767780780649b896be8
viKa1604 <vmur5048@mail.ru>
2026-03-14 10:13:40 +0000

    practice 1

---
+++ practice1/task1.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(){
+    int a = 10;
+    printf("Адрес переменной: %p\n", (void*)&a);
+    printf("Hello World!\n");
+    return 0;
+}
---
+++ practice1/task4.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {   
+
+    int numb = 10;   
+
+    printf("Значение переменной: %d\n", numb); 
+    printf("Адрес переменной: %p\n", &numb); 
+    
+    return 0;
+    
+}   
